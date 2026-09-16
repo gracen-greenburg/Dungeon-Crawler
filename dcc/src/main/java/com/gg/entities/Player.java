d88b1067c59297f73ae33b627a9f8f8cb6ef93ec
gracen-greenburg <gracenkg@email.sc.edu>
2026-09-16 13:20:16 -0400

    #2, #13 Creating Entity and Player classes

---
+++ dcc/src/main/java/com/gg/entities/Player.java
@@ -0,0 +1,16 @@
+package com.gg.entities;
+
+/**
+ * @author gracen-greenburg 2026
+ * 
+ *  Player Class is a type of entity that is controlled by the user
+ */
+public class Player extends Entity {
+
+    public Player(String name, int x, int y, int hp, int attack, int ac) {
+        super(name, x, y, hp, attack, ac);
+    }
+
+    // set up the specifics later on
+    
+}
